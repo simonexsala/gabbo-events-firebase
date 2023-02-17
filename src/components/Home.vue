61a776a64d772572150dac6caef2f8781c5b35f2
Simone Sala <simonexsala@gmail.com>
2023-02-17 22:20:20 +0100

    style changes and additions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -48,6 +48,27 @@
       </div>
     </ACard>
 
+<div class="stats bg-primary text-primary-content">
+  
+  <div class="stat">
+    <div class="stat-title">Account balance</div>
+    <div class="stat-value">$89,400</div>
+    <div class="stat-actions">
+      <button class="btn btn-sm btn-success">Add funds</button>
+    </div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-title">Current balance</div>
+    <div class="stat-value">$89,400</div>
+    <div class="stat-actions">
+      <button class="btn btn-sm">Withdrawal</button> 
+      <button class="btn btn-sm">deposit</button>
+    </div>
+  </div>
+  
+</div>
+
     <ACard
       :text="text"
       variant="fill"
