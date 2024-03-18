c96c03006936e7a59969b23bb59b86f5efe102c4
Simone Sala <simonexsala@gmail.com>
2024-03-18 19:05:36 +0100

    changes

--- functions/index.js
+++ functions/index.js
@@ -24,18 +24,21 @@ app.post('/', async (req, res) => {
       const db = admin.firestore();
 
       // const email = "simonexsala@gmail.com";
+      const shortCode = body.slice(-6);
       const email = body.resource.payer.email_address;
       const name = body.resource.payer.name;
       const time = body.resource.update_time;
       const amount = body.resource.purchase_units[0].amount.value;
       const [quantity, type] = body.resource.purchase_units[0].description.split(' ').slice(-2);
-      const capitalizedType = type.charAt(0).toUpperCase();
-      const qrCodeBody = `${body.id}-I${quantity}-T${capitalizedType}`;
+      // const capitalizedType = type.charAt(0).toUpperCase();
+      // const qrCodeBody = `${body.id}-I${quantity}-T${capitalizedType}`;
+      const qrCodeBody = body.id;
 
       const qrCode = await generateQRCode(qrCodeBody);
 
       await db.collection('evento').doc(body.id).set({
         code: qrCode,
+        shortCode: shortCode,
         data: time,
         email: email,
         nome: name,
@@ -44,27 +47,35 @@ app.post('/', async (req, res) => {
         totale: amount,
       });
 
-      // await db.collection('mail').doc(body.id).set({
-      //   to: email,
-      //   template: {
-      //     name: "prevendita",
-      //     data: {
-      //       nome: name.given_name,
-      //       numero: quantity,
-      //       path: qrCode,
-      //     },
-      //   },
-      // });
+      await db.collection('ingressi').doc(body.id).set({
+        shortCode: shortCode,
+        email: email,
+        numero: quantity,
+        used: 0,
+      });
+
       await db.collection('mail').doc(body.id).set({
         to: email,
         template: {
-          name: "lista",
+          name: "prevendita",
           data: {
             nome: name.given_name,
             numero: quantity,
+            shortCode: shortCode,
+            path: qrCode,
           },
         },
       });
+      // await db.collection('mail').doc(body.id).set({
+      //   to: email,
+      //   template: {
+      //     name: "lista",
+      //     data: {
+      //       nome: name.given_name,
+      //       numero: quantity,
+      //     },
+      //   },
+      // });
     } else {
       console.log(`Received event of type: ${body.event_type}`);
     }
