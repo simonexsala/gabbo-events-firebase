b5cd6fc9a8f1587ce03eb834177b22d321e4ce69
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2025-01-09 10:29:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,11 +11,12 @@ import './style.css'
 const app = createApp(App)
 app.config.globalProperties.$evento = {
   nome: 'Il Ghost',
-  data: 'Sabato 4 Gennaio',
-  luogo: 'Nova Club',
-  indirizzo: 'Via Alto Adige 83, Trento',
-  // tipologia: 'Open bar fino a mezzanotte',
-  tipologia: 'Ingresso con un drink',
+  data: 'Sabato 18 Gennaio',
+  luogo: 'Rosalpina',
+  indirizzo: 'Via Pianizza 27, Trento',
+  // indirizzo: 'Via Alto Adige 83, Trento',
+  tipologia: 'Open bar fino a mezzanotte',
+  // tipologia: 'Ingresso con un drink',
   // tipologia: 'Ticket ingresso',
   link: '',
 
